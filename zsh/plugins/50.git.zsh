9878bcd11f57525641363cd351d01c281bb4e257
Mark Grealish <mark@bhalash.com>
2024-01-14 12:20:20 +0000

    zsh: de-duplicate script, properly mark as private

--- zsh/plugins/50.git.zsh
+++ zsh/plugins/50.git.zsh
@@ -1,14 +1,13 @@
 #!/usr/bin/env zsh
 #
-# cd to git project root
+# cd to git project root folder
 
-function _is_git_project {
+function __is_git_project {
   git rev-parse 2> /dev/null
 }
 
-# cd to git project root folder
-function _cd_git_root {
-  if _is_git_project; then
+function __cd_git_root {
+  if __is_git_project; then
     builtin cd "$(git rev-parse --show-toplevel)"
     zle reset-prompt
     clear
@@ -21,19 +20,7 @@ function _cd_git_root {
   fi
 }
 
-function gr {
-  if _is_git_project; then
-    cd "$(git rev-parse --show-toplevel)"
-    clear
-
-    if [[ -n $TMUX ]]; then
-      tmux rename-window $(basename $PWD)
-    fi
-  else
-    echo "cd: not a git project: ${PWD}"
-  fi
-}
-
-# register widget and bind to ^h
-zle -N _cd_git_root
-bindkey '^H' _cd_git_root
+# bind to ^h
+zle -N __cd_git_root
+bindkey '^H' __cd_git_root
+alias gr=__cd_git_root
