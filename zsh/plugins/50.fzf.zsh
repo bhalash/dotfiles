c9c60e7a587450461435404eb13cf346bb94431c
Mark Grealish <mark@bhalash.com>
2024-01-30 10:57:10 +0000

    zsh: fix basic FZF integration for macOS
    - I had to add macOS completion files.
    - I needed to move ZSH plugin startup.

--- zsh/plugins/50.fzf.zsh
+++ zsh/plugins/50.fzf.zsh
@@ -4,22 +4,28 @@
 
 # ! FZF {{{
 
-# TODO(mark 2021-12-29): this whole section needs revision
 # See: https://github.com/junegunn/fzf#environment-variables
 export FZF_DEFAULT_COMMAND="rg --files --follow --hidden -g '!.git/*' -g '!.gitkeep'"
 export FZF_CTRL_T_COMMAND='rg --files --null | xargs -0 dirname | sort | uniq'
 export FZF_ALT_C_COMMAND='rg --files --null | xargs -0 dirname | sort | uniq'
 
+# Source FZF bindings and integration
 if [[ $OSTYPE =~ 'linux-gnu' ]]; then
   source '/usr/share/doc/fzf/examples/key-bindings.zsh'
   source '/usr/share/doc/fzf/examples/completion.zsh'
 fi
 
+if [[ $OSTYPE =~ 'darwin' ]]; then
+  source '/usr/local/opt/fzf/shell/key-bindings.zsh'
+  source '/usr/local/opt/fzf/shell/completion.zsh'
+fi
+
+# See: https://github.com/lincheney/fzf-tab-completion?tab=readme-ov-file
 source $DOTFILES_DIR/fzf-tab-completion/zsh/fzf-zsh-completion.sh
 
-# Widget: bind cd to <tab>, or fallthrough to fzf-zsh-completion {{{
+# Widget: bind cd to <tab> when the prompt is empty, or fallthrough to fzf-zsh-completion {{{
 
-function _tab_cd_or_fzf {
+function __tab_cd_or_fzf {
   if [[ $#BUFFER == 0 ]]; then
     fzf-cd-widget
   else
@@ -27,7 +33,7 @@ function _tab_cd_or_fzf {
   fi
 }
 
-zle -N _tab_cd_or_fzf
-bindkey '^I' _tab_cd_or_fzf
+zle -N __tab_cd_or_fzf
+bindkey '^I' __tab_cd_or_fzf
 
 # }}}
