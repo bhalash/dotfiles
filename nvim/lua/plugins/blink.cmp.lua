0808893c09cb368f89184012eb322dce6c795ef0
Mark Grealish <mark@bhalash.com>
2026-02-15 10:39:37 +0000

    vim: swap nvim-cmp -> blink.cmp
    nvim-cmp was annoying me - slow + many false positives.

---
+++ nvim/lua/plugins/blink.cmp.lua
@@ -0,0 +1,14 @@
+return {
+  'Saghen/blink.cmp',
+  dependencies = { 'rafamadriz/friendly-snippets' },
+  version = '1.*',
+  opts = {
+    keymap = { preset = 'super-tab' },
+    completion = { documentation = { auto_show = false } },
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
+    fuzzy = { implementation = 'prefer_rust_with_warning' }
+  },
+  opts_extend = { 'sources.default' }
+}
