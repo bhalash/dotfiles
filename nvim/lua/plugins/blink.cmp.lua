df248af1ef8e2f1afff943bf760bc5b6ffa7d007
Mark Grealish <mark@bhalash.com>
2026-02-26 22:50:29 +0000

    vim: work on blimp.cmp config
    Turns out I'm particular about how suggestions complete. On top of that
    I tuned it for performance in the monorepo at work.

--- nvim/lua/plugins/blink.cmp.lua
+++ nvim/lua/plugins/blink.cmp.lua
@@ -3,10 +3,38 @@ return {
   dependencies = { 'rafamadriz/friendly-snippets' },
   version = '1.*',
   opts = {
-    keymap = { preset = 'super-tab' },
-    completion = { documentation = { auto_show = false } },
+    keymap = {
+      ['<CR>'] = { 'accept', 'fallback' },
+      ['<Tab>'] = { 'snippet_forward', 'select_next', 'fallback' },
+      ['<S-Tab>'] = { 'snippet_backward', 'select_prev', 'fallback' },
+      ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+
+      ['<C-e'] = { 'hide', 'fallback' },
+      ['<Esc>'] = { 'hide', 'fallback' },
+      ['<Up>'] = { 'select_prev', 'fallback' },
+      ['<Down>'] = { 'select_next', 'fallback' },
+
+      ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+      ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+      ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+      ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+      ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
+    },
+    completion = {
+      keyword = {
+        range = 'prefix'
+      },
+      list = {
+        selection = {
+          preselect = true,
+          auto_insert = true,
+        },
+      },
+    },
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      -- INFO(mgrealis 2026-02-26): monorepo is hella slow with 'path' enabled
+      -- default = { 'lsp', 'path', 'snippets', 'buffer' },
+      default = { 'lsp', 'snippets', 'buffer' },
     },
     fuzzy = { implementation = 'prefer_rust_with_warning' }
   },
