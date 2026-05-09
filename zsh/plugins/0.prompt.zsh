1e2655bbc73726a041b3b3e0e915fde5930972a7
Mark Grealish <mark@bhalash.com>
2026-05-09 08:06:09 +0100

    zsh: pare down prompt code
    To be honest, I've always hated the red colour, but I so rarely worked
    as root that it never crossed my thought to fix it.

--- zsh/plugins/0.prompt.zsh
+++ zsh/plugins/0.prompt.zsh
@@ -1,52 +1,23 @@
 # Remove annoying "%" sign after certain outputs
-# See: https://stackoverflow.com/questions/13660636/what-is-percent-tilde-in-zsh
+# See: https://stackoverflow.com/questions/13660636
 setopt PROMPT_CR
 setopt PROMPT_SP
 export PROMPT_EOL_MARK=''
 
-# Wrap argument $1 in given fg and bg colours
-# See: http://zsh.sourceforge.net/Doc/Release/Prompt-Expansion.html#Visual-effects
-# See: https://www.calmar.ws/vim/256-xterm-24bit-rgb-color-chart.html
-function _prompt_color {
-  local CONTENT=$1
-  local FG=${2:-15} # fallback white
-  local BG=${3} # no fallback bg
-
-  if [[ -n $BG ]]; then
-    echo "%F{${FG}}%K{${BG}}${CONTENT}%f%k"
-  else
-    echo "%F{${FG}}${CONTENT}%f%k"
-  fi
-}
-
-# Separate segments of the prompt, eg foo@bar or foo/bar
-function _prompt_segment_separator {
-  local SEGMENT_SEPARATOR=$'\ue0b0'
-  local BG=${1:-0}
-  local FG=${2:-15}
-  echo $(_prompt_color $SEGMENT_SEPARATOR $FG $BG)
-}
-
-# Set username and colour for that user - red if root
+# Set username and hostname
 function _prompt_user {
-  local FG=15 # white
-
-  if [[ $(id -u) == 0 ]]; then
-    FG=9 # red
-  fi
-
-  echo $(_prompt_color '%n@%m ' $FG)
+  echo '%n@%m '
 }
 
-# Set dir in prompt
+# Set dir
 function _prompt_dir {
-  echo $(_prompt_color '%1~ ')
+  echo $'%1~ '
 }
 
 # Set vi mode - insert or command
 function _prompt_mode {
-  local MODE_NORMAL=$(_prompt_color '>')
-  local MODE_INSERT=$(_prompt_color '$')
+  local MODE_NORMAL='>'
+  local MODE_INSERT='$'
   echo "${${KEYMAP/vicmd/${MODE_NORMAL}}/(main|viins)/${MODE_INSERT}}"
 }
 
@@ -60,7 +31,7 @@ function _mode_cursor {
   fi
 }
 
-zle-line-init zle-keymap-select() {
+function zle-line-init zle-keymap-select {
   # Draw prompt
   PROMPT="$(_prompt_user)$(_prompt_dir)$(_prompt_mode) %b"
   RPROMPT=""
